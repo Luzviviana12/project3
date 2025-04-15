5eda8e447fdda91689031b9f68ed24f32e6d9d31
Luzviviana12 <lgonzalez12@hawk.iit.edu>
2025-04-14 21:52:12 -0500

    adding 3 map features

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+  const marker = new AdvancedMarkerElement({
+      map,
+      position: { lat: 41, lng: -87 },
+  });
 
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom,
+  center,
+});
+
+
+const {ColorScheme} = await google.maps.importLibrary("core")
+
+const mapOptions = {
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
+  colorScheme: ColorScheme.FOLLOW_SYSTEM,
+}
+map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
