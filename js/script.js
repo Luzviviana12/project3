c5b590fe38fccf4889aeb4d8aae4ab36b84409ea
Luzviviana12 <lgonzalez12@hawk.iit.edu>
2025-04-15 22:12:47 -0500

    adding slider

--- js/script.js
+++ js/script.js
@@ -28,3 +28,27 @@ const mapOptions = {
   colorScheme: ColorScheme.FOLLOW_SYSTEM,
 }
 map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+function init(){
+
+}
+window.addEventListener('load', init);
+
+function init(){
+  function myEventFunction(){
+  alert('Hello');
+}
+  var d4 = document.getElementById('div4');
+  d4.addEventListener('click', myEventFunction);
+  d4.addEventListener('click', function(){
+	console.log('I was Clicked!');
+	d4.style.backgroundColor = 'white';
+	d4.innerHTML = '<strong>Goodbye</strong>';
+});
+}
+window.addEventListener('load', init);
+
+$('.bxslider').bxSlider({
+  adaptiveHeight: true,
+  slideWidth: 960
+});
